e9422cd1629e2dd2e184f70d865396ad11f50601
Joletuar <96794364+Joletuar@users.noreply.github.com>
2023-01-30 21:07:55 +0000

    Final Proyecto

--- pages/admin.py
+++ pages/admin.py
@@ -5,5 +5,6 @@ from .models import *
 
 class PageAdmin(admin.ModelAdmin):
     readonly_fields = ['created', 'updated']
+    list_display = ('title', 'order')
 
 admin.site.register(Page, PageAdmin)
